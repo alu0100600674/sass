910b971148658c0dd480eb59f6cfa7e56603e066
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2014-11-27 22:25:25 +0000

    Creando menu

--- app.rb
+++ app.rb
@@ -4,3 +4,19 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+get '/ejemplos/ejemplo1' do
+  erb :ejemplo1
+end
+
+get '/ejemplos/ejemplo2' do
+  erb :ejemplo2
+end
+
+get '/ejemplos/ejemplo3' do
+  erb :ejemplo3
+end
+
+get '/ejemplos/ejemplo4' do
+  erb :ejemplo4
+end
